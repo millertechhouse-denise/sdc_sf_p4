0ccc1f7b8e8eb22bd7a05b3f439738d846701f2c
denise <denisej199@gmail.com>
2017-06-12 23:31:29 -0700

    add reflection

---
+++ Reflection.md
@@ -0,0 +1,20 @@
+# CarND-Controls-PID
+Self-Driving Car Engineer Nanodegree Program
+
+---
+
+## Reflections
+
+[video1]: ./PID.mp4 "Video"
+
+The PID parameters were set to:
+
+Proportional: 0.2
+There is a positional offset in the steering angle, and this is less than 1 to avoid overshoot.
+Integral: 0.005
+This is exptected to be small because there is no significant steady disturbanc.
+Derivative: 5
+This is expeted to be large because the error for the steering angle is large and can cause overshoot which this attempts to correct.
+
+[alt text][video1]
+
---
+++ src/PID.h
@@ -0,0 +1,60 @@
+#ifndef PID_H
+#define PID_H
+
+#include <uWS/uWS.h>
+
+class PID {
+public:
+  /*
+  * Errors
+  */
+  double p_error;
+  double i_error;
+  double d_error;
+
+  /*
+  * Coefficients
+  */ 
+  double Kp;
+  double Ki;
+  double Kd;
+	
+	
+	double integral_cte;
+	double prev_cte;
+	
+
+  /*
+  * Constructor
+  */
+  PID();
+
+  /*
+  * Destructor.
+  */
+  virtual ~PID();
+
+  /*
+  * Initialize PID.
+  */
+  void Init(double Kp, double Ki, double Kd);
+
+  /*
+  * Update the PID error variables given cross track error.
+  */
+  void UpdateError(double cte);
+
+  /*
+  * Calculate the total PID error.
+  */
+  double TotalError();
+	
+
+	
+	/*
+	*
+	*/
+	void Twiddle(double cte);
+};
+
+#endif /* PID_H */
